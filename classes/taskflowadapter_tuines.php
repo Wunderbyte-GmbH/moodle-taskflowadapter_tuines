f1cae69146af02d03bb78158331e84b5f55849b3
David Ala <davidala@Mac.dhw>
2025-07-01 16:41:14 +0200

    subpluginsettings for more adaptability #684

--- classes/taskflowadapter_tuines.php
+++ classes/taskflowadapter_tuines.php
@@ -25,10 +25,114 @@
 
 namespace taskflowadapter_tuines;
 
+use admin_setting_configselect;
+use admin_setting_configtext;
+use admin_setting_description;
+use admin_settingpage;
 use local_taskflow\plugininfo\taskflowadapter;
 
 /**
  * Class for the TUINES taskflow adapter.
  */
 class taskflowadapter_tuines extends taskflowadapter {
+    /**
+     * [Description for load_settings]
+     *
+     * @param \part_of_admin_tree $adminroot
+     * @param mixed $parentnodename
+     * @param mixed $hassiteconfig
+     *
+     * @return [type]
+     *
+     */
+    public function load_settings(\part_of_admin_tree $adminroot, $parentnodename, $hassiteconfig) {
+        if (!$hassiteconfig) {
+            return;
+        }
+        $componentname = 'taskflowadapter_tuines';
+        $allusercustomfields = profile_get_custom_fields();
+        $usercustomfields = [];
+        $settings = $adminroot->locate($parentnodename);
+        //@TODO: PLACEHOLDER FOR NOW make it constants.
+        $cohortlabelsettings = [
+           'translator_target_group_name' => get_string('name', $componentname),
+           'translator_target_group_description' => get_string('description', $componentname),
+           'translator_target_group_unitid' => get_string('unit', $componentname),
+        ];
+
+        $userlabelsettings = [
+            "" => "",
+            'translator_user_firstname' => get_string('firstname', $componentname),
+            'translator_user_lastname' => get_string('lastname', $componentname),
+            'translator_user_email' => get_string('email', $componentname),
+            'translator_user_units' => get_string('targetgroup', $componentname),
+            'translator_user_orgunit' => get_string('unit', $componentname),
+            'translator_user_supervisor' => get_string('supervisor', $componentname),
+            'translator_user_long_leave' => get_string('longleave', $componentname),
+            'translator_user_end' => get_string('contractend', $componentname),
+            'translator_user_internalid' => get_string('internalid', $componentname),
+        ];
+
+        if (!empty($allusercustomfields)) {
+            foreach ($allusercustomfields as $userprofilefield) {
+                $usercustomfields["{$userprofilefield->shortname}"] = $userprofilefield->name;
+            }
+        }
+        $validation = 1;
+        foreach ($usercustomfields as $key => $label) {
+            if (!empty(get_config($componentname, $key . '_translator'))) {
+                $validation++;
+            }
+        }
+        if ($validation < count($userlabelsettings)) {
+            $settings->add(
+                new admin_setting_description(
+                    $componentname . '/lessfunctions',
+                    '',
+                    get_string('lessfunctions', $componentname)
+                )
+            );
+        }
+        if ($validation > count($userlabelsettings)) {
+            $settings->add(
+                new admin_setting_description(
+                    $componentname . '/manyfunctions',
+                    '',
+                    get_string('manyfunctions', $componentname)
+                )
+            );
+        }
+
+        foreach ($usercustomfields as $key => $label) {
+            $settings->add(
+                new admin_setting_configtext(
+                    $componentname . '/' . $key,
+                    get_string('jsonkey', $componentname) . $label,
+                    get_string('enter_value', $componentname),
+                    '',
+                    PARAM_TEXT
+                )
+            );
+             $settings->add(
+                 new admin_setting_configselect(
+                     $componentname . '/' . $key . '_translator',
+                     get_string('function', $componentname) . $label,
+                     get_string('set:function', $componentname),
+                     "",
+                     $userlabelsettings,
+                 )
+             );
+        }
+        foreach ($cohortlabelsettings as $key => $label) {
+            $settings->add(
+                new admin_setting_configtext(
+                    $componentname . '/' . $key,
+                    $label,
+                    get_string('enter_value', $componentname),
+                    '',
+                    PARAM_TEXT
+                )
+            );
+        }
+    }
 }
