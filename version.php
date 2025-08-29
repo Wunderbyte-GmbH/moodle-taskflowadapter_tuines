80307b0fbb13d65ed5b94d43d44512ebc3a730f4
David Ala <davidala@Mac.dhw>
2025-08-29 15:12:30 +0200

    Version(2025082900)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025082502;
+$plugin->version   = 2025082900;
 $plugin->requires  = 2024042200; // Requires this Moodle version. Current: Moodle 4.4.
 $plugin->component = 'taskflowadapter_tuines';
 $plugin->supported = [401, 405];
